60adef404ce333488f26b611c62c096919196b75
Moonjuseok <wnwnwntjr@gmail.com>
2026-02-11 08:24:53 +0900

    0211테스트

---
+++ test1/git_test2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#define Tx_pin 3;
+#define Rx_pin 4;
+
+
+void main(){
+	int LED=0;
+	int Pressure=0;
+	int Temp=0;
+	PinOUT
+
+}
